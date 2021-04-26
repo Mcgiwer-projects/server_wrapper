c0326feb7cc808f88a3dd69458a7fad0eaf3c1aa
Michal D <154359+mdziczkowski@users.noreply.github.com>
2021-04-26 13:35:18 +0200

    Add files via upload
    Included headers

---
+++ headers/global.h
@@ -0,0 +1,24 @@
+/**
+	*This file is copyrighted with the CreativeCommons
+	* CA-NC-SA license. Please keep this entry in respect
+	* of the author's work. Thanks.
+	*
+	* @author Michal Dziczkowski <mcgiwer@prokonto.pl>
+	*
+	*/
+
+
+#ifndef _GLOBAL_H__
+#define _GLOBAL_H__
+
+#include "inc.h"
+
+typedef struct tcp_socket
+{
+    const family = AF_INET;
+    const iface = htonl(INADDR_ANY);
+    string address;
+    int port;
+};
+
+#endif
---
+++ headers/inc.h
@@ -0,0 +1,33 @@
+/**
+	*This file is copyrighted with the CreativeCommons
+	* CA-NC-SA license. Please keep this entry in respect
+	* of the author's work. Thanks.
+	*
+	* @author Michal Dziczkowski <mcgiwer@prokonto.pl>
+	*
+	*/
+
+#ifndef _INC_H__
+#define _INC_H__
+
+/* Global include */
+
+// System headers
+
+#include <stdio>
+#include <fstream>
+#include <iostream>
+#include <ios>
+
+// Network headers
+
+#include <sys/socket.h> // socket definitions
+#include <sys/types.h>  // socket types
+#include <sys/wait.h>   // for waitpid()
+#include <arpa/inet.h>  // inet (3) funtions
+#include <unistd.h>     // misc. UNIX functions
+#include <netdb.h>      // for getnameinfo()
+
+using namespace std;
+
+#endif // _INC_H__
